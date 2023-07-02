a232516313350aaf55c9b1d6b55f0559874ce99c
Darshan Jain <darshanjain.godha@gmail.com>
2023-07-02 21:57:17 +0530

    implemented the route for login and also implemented the token

--- DjBloodBank/controllers/authControllers.js
+++ DjBloodBank/controllers/authControllers.js
@@ -1,6 +1,6 @@
 const userModel = require("../models/userModel");
-
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const registerController = async (req, res) => {
   try {
@@ -34,4 +34,66 @@ const registerController = async (req, res) => {
   }
 };
 
-module.exports = { registerController };
+const loginController = async (req, res) => {
+  try {
+    const existingUser = await userModel.findOne({ email: req.body.email });
+
+    if (!existingUser) {
+      return res.status(404).send({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    const comparePassword = await bcrypt.compare(
+      req.body.password,
+      existingUser.password
+    );
+    if (!comparePassword) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+    const token = jwt.sign(
+      { userId: existingUser._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    return res.status(200).send({
+      success: true,
+      messsage: "Login Successfuly",
+      token,
+      existingUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in login api",
+      error,
+    });
+  }
+};
+
+const currentUserController = async (req, res) => {
+  try {
+    const user = await userModel.findOne({ _id: req.body.userId });
+
+    return res.status(200).send({
+      success: true,
+      message: "User fetched successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "unable to get current user",
+      error,
+    });
+  }
+};
+
+module.exports = { registerController, loginController, currentUserController };
--- DjBloodBank/routes/authRoutes.js
+++ DjBloodBank/routes/authRoutes.js
@@ -1,8 +1,17 @@
 const express = require("express");
-const { registerController } = require("../controllers/authControllers");
+const {
+  registerController,
+  loginController,
+  currentUserController,
+} = require("../controllers/authControllers");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.post("/register", registerController);
+
+router.post("/login", loginController);
+
+router.get("/currentuser", authMiddleware, currentUserController);
 
 module.exports = router;
